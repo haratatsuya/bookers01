1e945d5bdf5ed788eb50b99a99957afe5ee241e4
EC2 Default User <ec2-user@ip-172-31-26-18.ap-northeast-1.compute.internal>
2024-08-15 13:25:21 +0000

    3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,64 +1,49 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
-
   def index
-    @books = Book.all
-    @book = Book.new
+   @books = Book.all
+   @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
-      # DBへの保存成功
-      flash[:success] = "Book was successfully created."
+      flash[:complete]="Book was successfully created."
       redirect_to book_path(@book)
     else
-      # DBへの保存失敗
       @books = Book.all
-      render "index"
+      render :index
     end
   end
 
   def show
-    # @bookはbefore_action :set_bookで設定されています
+    @book = Book.find(params[:id])
   end
 
   def edit
-    # @bookはbefore_action :set_bookで設定されています
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
     if @book.update(book_params)
-      # DBの更新成功
-      flash[:success] = "Book was successfully updated."
+      flash[:complete]="Book was successfully updated."
       redirect_to book_path(@book)
     else
-      # DBの更新失敗
-      render "edit"
+      @books = Book.all
+      render :edit
     end
   end
 
   def destroy
-  @book = Book.find(params[:id])
-  logger.debug "Book to be destroyed: #{@book.inspect}"
-  if @book.destroy
-    flash[:success] = "Book was successfully destroyed."
-    logger.debug "Book destroyed successfully"
-  else
-    flash[:error] = "Failed to destroy the book."
-    logger.debug "Failed to destroy the book: #{@book.errors.full_messages}"
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:complete]="Book was successfully destroyed."
+      redirect_to "/books"
+    end
   end
-  redirect_to books_path
-end
-
-
 
   private
-def set_book
-  @book = Book.find(params[:id])
-end
-
-  def book_params
-    params.require(:book).permit(:title, :body)
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
   end
-end
